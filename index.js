486cafadcf441a39470f24ccc6df800ef15508ab
Rodrigo Santos <rodrigo.s.santos003@gmail.com>
2023-03-11 12:13:06 +0000

    Added main HTML page

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("www"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(8081, () => {
   console.log("Server running at http://localhost:8081");
